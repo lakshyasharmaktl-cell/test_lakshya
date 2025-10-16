9ff62dc451f2f77e3346f5c45d5c0a87a9d7b00c
Lakshya <lakshyasharmaktl@gmail.com>
2025-10-16 16:54:44 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-
+import { useState } from  'react'
+import { ChartBarDecreasing } from 'lucide-react';
+import { X } from 'lucide-react';
 export default function App() {
+  const [show, setshow] = useState(false)
   return (
-    <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
-      {/* Logo / Brand Name */}
-      <h1 className="text-xl font-bold">Aayush</h1>
+    <div>
 
-      {/* Navigation Links */}
-      <ul className="flex space-x-6">
-        <li><a href="#home" className="hover:text-gray-400">Home</a></li>
-        <li><a href="#about" className="hover:text-gray-400">About</a></li>
-        <li><a href="#services" className="hover:text-gray-400">Services</a></li>
-        <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
-      </ul>
-    </nav>
-  );
+      <nav className='flex justify-between text-center bg-green-600 py-3'>
+
+        <h1>Welcome to Lakshya Sharma</h1>
+
+        <ul className=' hidden md:flex gap-4 '>
+          <li>redtape</li>
+          <li>redtape</li>
+          <li>redtape</li>
+          <li>redtape</li>
+          <li>redtape</li>
+          <li>redtape</li>
+        </ul>
+
+        <div className='hidden md:flex gap-4'>
+          <button>sing up</button>
+          <button>sing in</button>
+        </div>
+
+        <div onClick={()=> setshow(!show)} className='md:hidden flex'>
+          {show ? <X /> : <ChartBarDecreasing />}
+        </div>
+      </nav>
+    </div>
+  )
 }
