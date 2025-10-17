086ef0cd769e6e6be807b0171cd44a806f83fa18
Lakshya <lakshyasharmaktl@gmail.com>
2025-10-17 15:21:45 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar.jsx'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div> <Navbar /></div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Navbar from './Components/Navbar/Navbar.jsx'
+
 import './index.css'
 import App from './App.jsx'
 
@@ -10,6 +10,6 @@ createRoot(document.getElementById('root')).render(
  <StrictMode>
     <App />
   </StrictMode>
-  <Navbar />
+ 
  </div>
 )
